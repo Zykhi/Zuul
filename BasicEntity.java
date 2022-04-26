5e57e7bbe2c23483079b6bdb223cdeda6a257d01
Charly <zykhioff@gmail.com>
2022-04-26 11:31:16 +0200

    update game engine

--- BasicEntity.java
+++
@@ -1,7 +0,0 @@
-public class BasicEntity extends Entity {
-    public BasicEntity() {
-        super(1, 1, 1, 1, 1, 1, false);
-        setName("???");
-    }
-
-}
--- GameEngine.java
+++ GameEngine.java
@@ -160,8 +160,6 @@ public class GameEngine {
     }
 
     private void createCharacter() {
-        BasicEntity vBasicEntity = new BasicEntity();
-        this.aRooms.get("mainmenu").setCharacter(vBasicEntity);
 
         Garret vGarret = new Garret();
         this.aRooms.get("lobby").setCharacter(vGarret);
